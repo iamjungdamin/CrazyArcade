7776ee5a81facfa6369177d7be6a1b3ea46c1035
iamjungdamin <jdm0366@naver.com>
2021-06-24 21:29:28 +0900

    SFML 첫번째 커밋

--- CrazyArcade/main.cpp
+++ CrazyArcade/main.cpp
@@ -1,6 +1,24 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Window");
+	RenderWindow window(sf::VideoMode(500, 500), "Window");
+	
+	RectangleShape rs;
+
+	rs.setFillColor(Color::Green);
+	rs.setSize(Vector2f(100.f, 100.f));
+	rs.setPosition(Vector2f(250.f, 250.f));
+	rs.setOrigin(rs.getSize() / 2.f);
+
+	while (window.isOpen())
+	{
+		window.clear();
+
+		//rs.setPosition(rs.getPosition().x + .5f, rs.getPosition().y);
+		window.draw(rs);
+		window.display();
+	}
 }
