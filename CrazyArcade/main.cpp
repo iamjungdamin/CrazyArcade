73be34b758c96909c84f6e8878d6fd106328b3e3
iamjungdamin <jdm0366@naver.com>
2021-06-24 22:06:16 +0900

    스프라이트 시트 잘라보기

--- CrazyArcade/main.cpp
+++ CrazyArcade/main.cpp
@@ -5,22 +5,51 @@ using namespace sf;
 int main()
 {
 	RenderWindow window(sf::VideoMode(500, 500), "Window");
+	window.setFramerateLimit(60);
 	
-	Sprite sp;
-	Texture tx;
-	tx.loadFromFile("Textures/hi.png");
+	IntRect txSq(0, 0, 319 / 3, 424 / 4);
 
-	sp.setScale(1.2f, 1.2f);
-	sp.setOrigin(tx.getSize().x / 2.f, tx.getSize().y / 2.f);
-	sp.setTexture(tx);
-	sp.setPosition(250.f, 250.f);
+	Texture tx;
+	tx.loadFromFile("Textures/DragonFrames.png"); //319*424
+
+	Sprite sp(tx, txSq);
+	sp.setScale(3.f, 3.f);
+
+	Clock clock;
 
 	while (window.isOpen())
 	{
 		window.clear();
+		
+		Event e;
+		while (window.pollEvent(e))
+		{
+			switch (e.type)
+			{
+			case Event::Closed:
+				window.close();
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		if (clock.getElapsedTime().asSeconds() >= 0.3f) //0.3ÃÊ
+		{
+			if (txSq.left >= 212)
+			{
+				txSq.left = 0;
+			}
+			else
+			{
+				txSq.left += 319 / 3;
+			}
+			sp.setTextureRect(txSq);
+			clock.restart();
+		}
 
 		window.draw(sp);
-
 		window.display();
 	}
 }
