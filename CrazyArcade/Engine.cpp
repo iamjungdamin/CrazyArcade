78208550113559e4e16af7c7b91177fba56cf5dc
iamjungdamin <jdm0366@naver.com>
2021-07-01 01:59:36 +0900

    입력 받아보기
    + 아이콘 설정, window->set... 함수들 살펴보기

--- CrazyArcade/Engine.cpp
+++ CrazyArcade/Engine.cpp
@@ -13,6 +13,10 @@ Engine::~Engine()
 void Engine::Init()
 {
 	this->window = new RenderWindow(VideoMode(500, 500), "Window");
+	window->setMouseCursorVisible(false);
+	Image icon;
+	icon.loadFromFile("Textures/icon.jpg");
+	window->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 }
 
 void Engine::Destroy()
@@ -33,9 +37,40 @@ void Engine::Input()
 			window->close();
 			break;
 
+		case Event::KeyPressed:
+			switch (evt.key.code)
+			{
+			case Keyboard::A:
+				cout << "Pressed A key !!\n";
+				break;
+
+			default:
+				break;
+			}
+
 		default:
 			break;
 		}
+	}
+
+	// Keyboard Input
+	if (Keyboard::isKeyPressed(Keyboard::Escape))
+	{
+		window->close();
+	}
+
+	// Mouse Input
+	if (Mouse::isButtonPressed(Mouse::Left))
+	{
+		window->setTitle("Left Click");
+	}
+	else if (Mouse::isButtonPressed(Mouse::Right))
+	{
+		window->setTitle("Right Click");
+	}
+	else
+	{
+		window->setTitle("Window");
 	}
 }
 
